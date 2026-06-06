876af1e5c700feb383ea931e8d885b3d60f2bbfe
Raghav Purohit <raghavpurohit71@gmail.com>
2026-06-06 18:53:59 +0530

    changed test validator

--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -1,11 +1,9 @@
-from app.utils.validators import (
-    validate_cgpa
-)
+from app.utils.validators import Validator
 
 
 def test_validator():
 
-    result = validate_cgpa(8.5)
+    result = Validator.validate_cgpa(8.5)
 
     print("\n========== VALIDATOR OUTPUT ==========")
     print("CGPA Validation Result:", result)
